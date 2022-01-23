3c201cbcdeefa1739f08360ac8b5f42a7732b25d
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2022-01-22 23:01:43 -0800

    lsat one

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,6 @@ from . import views
 urlpatterns = [
   path('',views.lobby),
   path('room/',views.room),
-  path('lobby/',views.lobby),
   path('get_token/',views.getToken),
   path('create_member/', views.createMember),
   path('get_member/', views.getMember),
